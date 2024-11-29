0401258bc4698140d559eff9b7716d57b096605d
balwan coding <balwansinghrajput1708@gmail.com>
2024-11-29 18:06:49 +0530

    image resp

--- src/MedicineCart.jsx
+++ src/MedicineCart.jsx
@@ -4,7 +4,13 @@ import { Link } from "react-router-dom";
 function MedicineCart({ image, title, details, price, id }) {
   return (
     <div className="flex items-center justify-center gap-2 p-3 shadow-2xl sm:m-3 md:m-3 lg:m-3 hover:translate-y-3 rounded-3xl bg-slate-100 w-80 md:w-96 sm:w-80 lg:w-96 ">
-      <img className="h-20 rounded-lg" src={image} alt={title} />
+      <div className="w-full h-20 aspect-square">
+        <img
+          className="object-cover w-full h-full rounded"
+          src={image}
+          alt={title}
+        />
+      </div>
       <div className="m-1">
         <h1 className="text-blue-700">{title}</h1>
         <p className="text-red-500">{details}</p>
